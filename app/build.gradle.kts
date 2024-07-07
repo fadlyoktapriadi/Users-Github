e61eeaa0b1e36f05f1d9f9965794b6133811f664
fadlyoktapriadi <fadlyoktapriadi41@gmail.com>
2024-07-07 20:18:41 +0700

    delete google play dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ android {
 dependencies {
 
     implementation (project(":core"))
-    implementation ("com.google.android.play:core:1.10.3")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
 }
