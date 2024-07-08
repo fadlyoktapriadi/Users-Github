ef2db44c99ea9d2e03f69ed258e83fac6c817fee
fadlyoktapriadi <fadlyoktapriadi41@gmail.com>
2024-07-08 11:42:49 +0700

    Add obfuscation ProGuard

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -22,12 +22,21 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+
+        debug {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
